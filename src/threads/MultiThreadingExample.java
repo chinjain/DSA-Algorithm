f37989e9c8c019bc175149a990159bae17d95d50
chinmay1.jain <chinmay1.jain@ril.com>
2024-06-19 00:47:46 +0530

    Final Commit

--- src/threads/MultiThreadingExample.java
+++ src/threads/MultiThreadingExample.java
@@ -8,25 +8,33 @@ package threads;
  */
 
 // Case 4 multi task with multi threads 
-public class MultiThreadingExample implements Runnable {
+public class MultiThreadingExample  {
 
-	@Override
-	public void run() {
-		System.err.println("running");
-	}
+//	@Override
+//	public void run() {
+//		System.err.println("running");
+//	}
 
 	public static void main(String[] args) {
-		MultiThreadingExample example = new MultiThreadingExample();
-		Thread t1 = new Thread(example);
-		Mythread1 mythread1 = new Mythread1();
-		mythread1.start();
-		mythread1.setName("abcd");
-		Mythread2 mt2 = new Mythread2();
-		mt2.start();
-		System.out.println(t1.isAlive());
-		t1.start();
-		Thread.currentThread().setName("Chinmay's-Thread");
-//		System.out.println(10/0);
+//		MultiThreadingExample example = new MultiThreadingExample();
+//		Thread t1 = new Thread(example);
+//		Mythread1 mythread1 = new Mythread1();
+//		mythread1.start();
+//		mythread1.setName("abcd");
+//		Mythread2 mt2 = new Mythread2();
+//		mt2.start();
+//		System.out.println(t1.isAlive());
+//		t1.start();
+//		Thread.currentThread().setName("Chinmay's-Thread");
+////		System.out.println(10/0);
+		
+	Mythread1 t1 = new Mythread1();
+	Mythread2 t2 = new Mythread2();
+	
+	t1.start();
+	t2.start();
+	System.out.println("main");
+	
 	}
 
 }
@@ -35,9 +43,14 @@ class Mythread1 extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("Thread 1 running");
+//		System.out.println("Thread 1 running");
+//		
+//		System.out.println(Thread.currentThread().getName());
 		
-		System.out.println(Thread.currentThread().getName());
+		while(true) {
+			System.out.print("A");
+			sleep(100);
+		}
 	}
 
 }
@@ -46,7 +59,10 @@ class Mythread2 extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("Thread 2 running");
+//		System.out.println("Thread 2 running");
+		while(true) {
+			System.out.print("b");
+		}
 	}
 
 }
