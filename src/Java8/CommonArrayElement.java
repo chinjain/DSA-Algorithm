cd61642a65c8da038d12f277384e7a4e86534276
chinmay jain <chinmayjain03@gmail.com>
2024-12-17 22:07:45 +0530

    Code Refractor

--- src/Java8/CommonArrayElement.java
+++ src/Java8/CommonArrayElement.java
@@ -1,7 +1,6 @@
 package Java8;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class CommonArrayElement {
 
--- src/threads/RunAsynchDemo.java
+++ src/threads/RunAsynchDemo.java
@@ -1,39 +1,39 @@
-package threads;
-
-import java.io.File;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class RunAsynchDemo {
-
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-
-		File file = new File("/DefaultDevTesting/employee.json");
-		RunAsynchDemo.saveEmp(file);
-
-	}
-
-	public static Void saveEmp(File jsonFile) throws InterruptedException, ExecutionException {
-
-		ObjectMapper mapper = new ObjectMapper();
-		CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> {
-			try {
-
-				// List<EmployeeJson> jsons = mapper.readValue(jsonFile, new
-				// TypeReference<List<EmployeeJson>>() {
-				// });
-				//
-				// System.out.println("thread :" + Thread.currentThread().getName());
-				// jsons.stream().forEach(System.out::println);
-			} catch (Exception e) {
-
-				e.printStackTrace();
-
-			}
-		});
-		return runAsync.get();
-	}
-
-}
+//package threads;
+//
+//import java.io.File;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ExecutionException;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//public class RunAsynchDemo {
+//
+//	public static void main(String[] args) throws InterruptedException, ExecutionException {
+//
+//		File file = new File("/DefaultDevTesting/employee.json");
+//		RunAsynchDemo.saveEmp(file);
+//
+//	}
+//
+//	public static Void saveEmp(File jsonFile) throws InterruptedException, ExecutionException {
+//
+//		ObjectMapper mapper = new ObjectMapper();
+//		CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> {
+//			try {
+//
+//				// List<EmployeeJson> jsons = mapper.readValue(jsonFile, new
+//				// TypeReference<List<EmployeeJson>>() {
+//				// });
+//				//
+//				// System.out.println("thread :" + Thread.currentThread().getName());
+//				// jsons.stream().forEach(System.out::println);
+//			} catch (Exception e) {
+//
+//				e.printStackTrace();
+//
+//			}
+//		});
+//		return runAsync.get();
+//	}
+//
+//}
